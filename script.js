5717e9c7fbc55d98095de937dfba9aa29d2f4d1b
Jaden Zhou <108704365+JadenZhou@users.noreply.github.com>
2025-02-15 20:43:36 -0500

    feat: step 2 done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+
+  switch (choice) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
